70d975ae87101d5f19af0a8756298cde1612c135
Eliezer-2002 <eliezerfsdev@gmail.com>
2026-02-27 12:38:11 +0530

    Modify Code

--- user_management/settings.py
+++ user_management/settings.py
@@ -100,6 +100,14 @@ DATABASES = {
     )
 }
 
+# If DATABASE_URL exists (Render production), override
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
